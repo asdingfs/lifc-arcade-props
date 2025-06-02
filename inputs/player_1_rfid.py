9121f8c76b6c5771d6d139341973a81b6deebaf8
Anthony Setiawan <anthonysetiawan.ding@gmail.com>
2025-06-03 03:33:16 +0800

    Attempt to implement 2 rfid readers for different players

---
+++ inputs/player_1_rfid.py
@@ -0,0 +1,35 @@
+#!/home/trigger/lifc-arcade-props/inputs/.venv/bin/python
+"""
+    based on example from: pn532pi library
+    To enable debug message, set DEBUG in nfc/PN532_log.h
+"""
+import logging
+import time
+import os
+from pn532pi import Pn532, pn532, Pn532Spi
+from rfid_reader import setup, read, log
+from constants import PLAYER_1_RFID_SS_PIN
+
+# setup logging
+script_dir = os.path.dirname(os.path.abspath(__file__))
+logging.basicConfig(
+    filename=os.path.join(script_dir, 'readers.log'),
+    level=logging.INFO,  # Log level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    format='%(asctime)s %(levelname)s: %(message)s'
+)
+
+logger = logging.getLogger(__name__)
+
+# setup player 1 RFID reader SPI
+PN532_SPI = Pn532Spi(PLAYER_1_RFID_SS_PIN)
+nfc = Pn532(PN532_SPI)
+setup(nfc, PLAYER_1_RFID_SS_PIN, logger)
+
+while True:
+  uid = read(nfc, PLAYER_1_RFID_SS_PIN, logger)
+  if uid:
+    # TODO: process the UID
+    pass
+  else:
+    log(logger.error, PLAYER_1_RFID_SS_PIN, "No card detected or read failed.")
+  time.sleep(2)  # wait before next read attempt
--- inputs/rfid_reader.py
+++ inputs/rfid_reader.py
@@ -11,24 +11,23 @@ import binascii
 
 from pn532pi import Pn532, pn532, Pn532Spi
 
-# Set the desired interface to True
-SPI = False
 
-if SPI:
-  PN532_SPI = Pn532Spi(Pn532Spi.SS0_GPIO8)
-  nfc = Pn532(PN532_SPI)
+def log(logger_level, ss_pin, message):
+  logger_level(f"[Reader-SS#{ss_pin}]: {message}")
 
 
-def setup():
+def setup(nfc, ss_pin, logger):
   nfc.begin()
 
+  # print out firmware version
   firmware = nfc.getFirmwareVersion()
   if not firmware:
-    print("Didn't find PN53x board")
-    raise RuntimeError("Didn't find PN53x board")  # halt
+    log(logger.error, ss_pin, "Didn't find PN53x board")
+    return False
 
-  # Got ok data, print it out!
-  print(
+  # print firmware version
+  log(
+      logger.info, ss_pin,
       "Found chip PN5 {:#x} Firmware ver. {:d}.{:d}".format(
           (firmware >> 24) & 0xFF, (firmware >> 16) & 0xFF,
           (firmware >> 8) & 0xFF
@@ -41,31 +40,25 @@ def setup():
 
   # configure board to read RFID tags
   nfc.SAMConfig()
+  log(
+      logger.info, ss_pin,
+      "Successfully setup! Waiting for an ISO14443A card..."
+  )
+  return True  # indicates setup was successful
 
-  print("Waiting for an ISO14443A card...")
 
-
-def loop():
+def read(nfc, ss_pin, logger):
   # Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
   # 'uid' will be populated with the UID, and uidLength will indicate
   # if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
   success, uid = nfc.readPassiveTargetID(pn532.PN532_MIFARE_ISO14443A_106KBPS)
 
   if success:
-    print("Found a card!")
-    print("UID Length: {:d}".format(len(uid)))
-    print("UID Value: {}".format(binascii.hexlify(uid)))
-    # Wait 1 second before continuing
-    time.sleep(1)
-    return True
+    log(logger.info, ss_pin, "Found a tag!")
+    log(logger.info, ss_pin, "UID Length: {:d}".format(len(uid)))
+    log(logger.info, ss_pin, "UID Value: {}".format(binascii.hexlify(uid)))
+    return binascii.hexlify(uid).decode('utf-8')  # return UID as a string
   else:
     # pn532 probably timed out waiting for a card
-    print("Timed out waiting for a card!")
-    return False
-
-
-if __name__ == '__main__':
-  setup()
-  found = loop()
-  while not found:
-    found = loop()
+    log(logger.info, ss_pin, "Timed out waiting for a card!")
+    return None
