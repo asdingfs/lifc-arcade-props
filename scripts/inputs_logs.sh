42b86c2b2adeb7838d2b70e82fd5b3eed0ce30c0
Anthony Setiawan <anthonysetiawan.ding@gmail.com>
2025-06-06 20:41:29 +0800

    Make logs folder location automatic

--- scripts/inputs_logs.sh
+++ scripts/inputs_logs.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-sudo mkdir -p /var/log/inputs
 sudo tail -f /var/log/inputs/readers.log /var/log/inputs/buttons.log
--- scripts/server_logs.sh
+++ scripts/server_logs.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-sudo mkdir -p /var/log/nginx /var/log/gunicorn
 sudo tail -f /var/log/nginx/access.log /var/log/nginx/error.log \
   /var/log/gunicorn/access.log /var/log/gunicorn/error.log
