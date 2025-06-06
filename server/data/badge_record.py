48278f3c075cdbb7f2c133f80cf626a684a5ff3e
Anthony Setiawan <anthonysetiawan.ding@gmail.com>
2025-06-07 01:44:19 +0800

    Add arcade styling and fix missing created_at in players creation page

--- server/data/badge_record.py
+++ server/data/badge_record.py
@@ -1,5 +1,6 @@
 from sqlite3 import Row
 from server.utils import uploaded_file_path, arcadeify
+from datetime import datetime
 
 
 class BadgeRecord:
@@ -10,17 +11,21 @@ class BadgeRecord:
       name: str,
       media_id: int | None,
       img_src: str | None,
+      created_at: datetime | None = None,
+      updated_at: datetime | None = None,
   ):
     self.pkey = pkey
     self.code = code
     self.name = name
     self.media_id = media_id
     self.img_src = img_src
+    self.created_at = created_at if created_at else datetime.now()
+    self.updated_at = updated_at if updated_at else datetime.now()
     self.arcadeify_names()
 
   def __repr__(self):
     return (f"BadgeRecord("
-            f"id={self.pkey}, uuid={self.code}, "
+            f"pkey={self.pkey}, uuid={self.code}, "
             f"name={self.name}, media_id={self.media_id}"
             f")")
 
@@ -37,7 +42,9 @@ class BadgeRecord:
         code=row["code"],
         name=row["name"],
         media_id=int(row["media_id"]) if row["media_id"] else None,
-        img_src=row["img_src"] if row["img_src"] else None
+        img_src=row["img_src"] if row["img_src"] else None,
+        created_at=row["created_at"] if row["created_at"] else None,
+        updated_at=row["updated_at"] if row["updated_at"] else None,
     )
 
   @classmethod
@@ -49,4 +56,6 @@ class BadgeRecord:
         name=request.form.get("name"),
         media_id=int(media_id) if media_id else None,
         img_src=None,  # when creating from request, this is unnecessary
+        created_at=None,
+        updated_at=None,
     )
